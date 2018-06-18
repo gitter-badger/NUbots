66b0a8ff191e4882aa19be2f805da7348b0fe4ae
Alex Biddulph <c3124185@uon.edu.au>
2018-06-18 16:49:14 -0400

    Enables walk engine stand script generation

--- module/motion/OldWalkEngine/src/OldWalkEngine.cpp
+++ module/motion/OldWalkEngine/src/OldWalkEngine.cpp
@@ -244,11 +244,11 @@ namespace motion {
             emit(std::make_unique<WalkConfigSaved>());
         });
 
-        generateStandScriptReaction = on<Trigger<Sensors>, Single>().then([this](const Sensors& /*sensors*/) {
+        generateStandScriptReaction = on<Trigger<Sensors>, Single>().then([this](const Sensors& sensors) {
             generateStandScriptReaction.disable();
-            // generateAndSaveStandScript(sensors);
-            // state = State::LAST_STEP;
-            // start();
+            generateAndSaveStandScript(sensors);
+            state = State::LAST_STEP;
+            start();
         });
 
         reset();
